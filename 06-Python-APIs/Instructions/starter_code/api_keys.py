f7ab7de77d26ba0891de2c8d7743ac6778011d91
Andrew Lau <drewlau@gmail.com>
2020-04-05 18:50:45 -0400

    Added open weather and google API keys

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9e190a5be204b56137d08bb5ea90fb7a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDbXUsS4GJ6c4HPbpOxfTOGlFbP2SH8vjA"
